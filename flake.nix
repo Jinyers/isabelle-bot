05f93de60b8afc47ce2a84479649a5b5ddd43622
Jinyer <reper-2014.ru@yandex.ru>
2024-12-31 18:12:42 +0300

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Isabelle Discord Bot";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+  	system = "x86_64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+	{
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+				rustup
+			];
+      shellHook = "exec zsh";
+		};
+	};
+}
