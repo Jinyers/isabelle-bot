7a9bef994880412cd1a177d1d8f2ab687db820ba
Jinyer <reper-2014.ru@yandex.ru>
2025-01-06 03:35:14 +0300

    Renamed `create_embeded_profile` to `create_profile_reply`
    I'm not sure that I need to know about embeded, but I need to know that
    it Reply

--- src/commands/profile.rs
+++ src/commands/profile.rs
@@ -3,7 +3,7 @@ use poise::serenity_prelude as serenity;
 use poise::{command, CreateReply};
 use tracing::error;
 
-fn create_embeded_profile(user: &serenity::User) -> CreateReply {
+fn create_profile_reply(user: &serenity::User) -> CreateReply {
     let embed = serenity::CreateEmbed::new()
         .title("NookLink")
         .description(format!("Пользователь {}", serenity::Mention::from(user.id)));
@@ -17,7 +17,7 @@ pub async fn profile(
     user: Option<serenity::User>
     ) -> Result<(), Error> {
     let user = user.as_ref().unwrap_or_else(|| ctx.author());
-    let result = ctx.send(create_embeded_profile(user)).await;
+    let result = ctx.send(create_profile_reply(user)).await;
     if let Err(why) = result {
         error!("Cannot respond {} because {}", user.name, why);
     }
@@ -29,7 +29,7 @@ pub async fn profile_context_menu(
     ctx: Context<'_>,
     user: serenity::User
     ) -> Result<(), Error> {
-    let result = ctx.send(create_embeded_profile(&user)).await;
+    let result = ctx.send(create_profile_reply(&user)).await;
     if let Err(why) = result {
         error!("Cannot respond {} because {}", user.name, why);
     }
